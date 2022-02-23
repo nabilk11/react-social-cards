bd6bef8dbd1159bbe5681f1de0e92f5b7663a750
nabilk11 <khan.nabiln11@gmail.com>
2022-02-23 00:27:13 -0500

    added user filtering with search bar on app component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ function App() {
 // state for users
   const [users, setUsers] = useState([]);
 
+// state for ALL users
+  const [allUsers, setAllUsers] = useState([]);
+
 // API fetch call within useEffect hook
 useEffect(async() => {
   let userData
@@ -17,14 +20,28 @@ useEffect(async() => {
       console.log(`Error is API Call: ${error}`)
       userData = [];
     }
+    setAllUsers(userData)
     setUsers(userData)
-   console.log(users)
+   
 
 }, [])
+
+//FILTERCARDS FUNCTION
+const filterCards = (e) => {
+  const value = e.target.value.toLowerCase();
+  const filteredUsers = allUsers.filter(
+    user => (`${user.name.first} ${user.name.last}`
+    .toLowerCase()
+    .includes(value)
+    )
+  )
+  setUsers(filteredUsers)
+}
 
   return (
     <div className="App">
       <h1>Social Cards</h1>
+      <input className='search-box' type="text" placeholder='Search...' onInput={filterCards}/>
       <div className="cards-container">
       {users.map((user, index) => (
         <SocialCard userData={user} key={index}/>
